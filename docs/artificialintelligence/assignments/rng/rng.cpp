f8e99ed4b4ad66261210df2ddaef573c4006f84b
Lucas Bonanno <lucas.bonanno@mymail.champlain.edu>
2024-09-22 23:32:38 -0400

    rng progress

--- docs/artificialintelligence/assignments/rng/rng.cpp
+++ docs/artificialintelligence/assignments/rng/rng.cpp
@@ -2,18 +2,67 @@
 #include <fstream>
 #include <iostream>
 #include <istream>
+#include <unordered_set>
 const std::string TEST_FOLDER = "\\tests\\";
 unsigned int xorShift(unsigned int seed, int r1, int r2);
+
+struct State {
+  unsigned int x, y;
+
+  bool operator==(const State& other) const {
+    return x == other.x && y == other.y;
+  }
+};
+
+namespace std {
+template<>
+  struct hash<State> {
+    std::size_t operator()(const State& state) const { return state.x ^ state.y; }
+  };
+}
+
 int main(){
   // code here
+
+  int cycles = 0;
+  int warmupPhase = 0;
+  int numOfStates = 0;
+
+  std::unordered_set<State> states;
+State state = {0,0};
+
+
   unsigned int seed, N, min, max;
   std::cin >> seed >> N >> min >> max;
   unsigned int i;
+unsigned int newSeed;
+  state.x = seed;
+
+
   for(i = N; i >= 1; i--)
   {
     //Run xor shift
-    seed = xorShift(seed, min, max);
+    state.y = xorShift(state.x, min, max);
+
+
+
+
+    if (!states.contains(state)) {
+      warmupPhase++;
+    }
+
+    states.insert({state});
+
+
+     numOfStates = states.count(state);
+    if (numOfStates == 2) {
+      cycles++;
+    }
+    state.x = state.y;
   }
+  warmupPhase -= cycles;
+  std::cout << "the warmup period is: " << warmupPhase << std::endl;
+  std::cout << "the cycle period is: " << cycles << std::endl;
 }
 //The purpose of this function is to take the number and xor shift it to output a pseudo-random number
     unsigned int xorShift(unsigned int seed, int r1, int r2)
